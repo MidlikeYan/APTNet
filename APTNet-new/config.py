870880d27cc5c4bfb7ce46a7468f39c22a1523e7
MidlikeYan <168733702+MidlikeYan@users.noreply.github.com>
2024-09-06 06:30:29 +0800

    Add files via upload

---
+++ APTNet-new/config.py
@@ -0,0 +1,24 @@
+# -----------------
+# DATASET ROOTS
+# -----------------
+cifar_10_root = '/disk/datasets'
+cifar_100_root = '/autodl-tmp/cifar100'
+herbarium_dataroot = '/disk/datasets/herbarium_19'
+imagenet_root = '/disk/datasets/ILSVRC12'
+
+cub_root = '/root/autodl-tmp/CUB'
+
+aircraft_root = '/disk/datasets/ood_zoo/ood_data/aircraft/fgvc-aircraft-2013b'
+
+car_root = '/disk/datasets/ood_zoo/ood_data/stanford_car'
+scars_meta_path = "/disk/datasets/ood_zoo/ood_data/stanford_car/devkit/cars_{}.mat"
+
+# OSR Split dir
+osr_split_dir = 'data/ssb_splits'
+
+# -----------------
+# OTHER PATHS
+# -----------------
+dino_pretrain_path = '/root/pretrained-models/dino_vitbase16_pretrain.pth' 
+clip_pretrain_path = '/disk/work/hjwang/pretrained_models/clip/ViT-B-16.pt' 
+feature_extract_dir = '/root/work/extracted_features_public_impl'     # Extract features to this directory
